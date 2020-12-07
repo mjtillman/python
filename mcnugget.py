1aeb5bce3af0467ba8e23b080edbb168efb04095
mjtillman <mjtillman@gmail.com>
2020-12-06 22:55:01 -0600

    edit of original nugget

--- mcnugget.py
+++ mcnugget.py
@@ -5,36 +5,44 @@ nug = int(input("How many McNuggets would you like to buy? "))
 num = nug
 
 i, j, k, l, m = 0, 0, 0, 0, 0
-
-sols = []
           
-if num > 40:
+if num > 0:
     i = num // 40
     num = nug - (i * 40)
-    print(i, "40-piece,")
 
-if num > 20:
-    j = i % 20
+if num >= 20:
+    j = num // 20
     num -= j * 20
-    print(j, "20-piece,")
 
-if num > 10:
-    k = j % 10
+if num >= 10:
+    k = num // 10
     num -= k * 10
-    print(k, "10-piece,")
 
-if num > 6:
-    l = k % 6
+if num >= 6:
+    l = num // 6
     num -= l * 6
-    print(l, "6-piece,")
 
-if num > 4:
-    m = l % 4
+if num >= 4:
+    m = num // 4
     num -= m * 4
-    print(m, "4-piece,")
 
 if num > 0:
     print("You have", num, "nugget(s) left, you can't buy", nug, "nuggets from McDonald's")
-    
-    
-    
+else:
+    print("For", nug, "nuggets, purchase:")
+
+    if i > 0:
+        print(i, "40-piece(s),")
+
+    if j > 0:
+        print(j, "20-piece(s),")
+
+    if k > 0:
+        print(k, "10-piece(s),")
+        
+    if l > 0:
+        print(l, "6-piece(s),")
+        
+    if m > 0:
+        print(m, "4-piece(s),")
+        
