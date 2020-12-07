a32e9451eae61e311c2d229a9ac928c3c99a14f6
mjtillman <mjtillman@gmail.com>
2020-12-06 20:33:54 -0600

    started mcnugget problem

---
+++ mcnugget.py
@@ -0,0 +1,20 @@
+print("McNugget Problem")
+
+num = int(input("How many McNuggets would you like to buy?")
+foo = num
+          
+if num > 40:
+    foo = num // 40
+    i = num % 40
+
+if i > 20:
+    j = i % 20
+
+if j > 10
+    k = j % 10
+
+if k > 6
+    l = k % 6
+
+if l > 4
+    m = l % 4
